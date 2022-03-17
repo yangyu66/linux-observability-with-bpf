d34a3c894c9fa4c7762be09bb4fbda4582d82b48
yang <yangyu0627@gmail.com>
2022-03-16 20:43:58 -0700

    use prototype-kernel

--- code/chapter-2/another_way/bpf_program.c
+++ code/chapter-2/another_way/bpf_program.c
@@ -1,8 +1,13 @@
-#include <linux/bpf.h>
+// #include <linux/bpf.h>
+#include <uapi/linux/bpf.h>
+#include "prototype-kernel/kernel/samples/bpf/bpf_helpers.h"
 #define SEC(NAME) __attribute__((section(NAME), used))
 
-static int (*bpf_trace_printk)(const char *fmt, int fmt_size,
-                               ...) = (void *)BPF_FUNC_trace_printk;
+// see https://blog.raymond.burkholder.net/index.php?/archives/1000-eBPF-Basics.html
+// clang  -I/usr/src/linux-5.4.100/include -O2 -target bpf -c bpf_program.c -I/usr/src/linux-5.4.100/tools/testing/selftests/bpf -o bpf_program.o
+
+// static int (*bpf_trace_printk)(const char *fmt, int fmt_size,
+//                                ...) = (void *)BPF_FUNC_trace_printk;
 
 SEC("tracepoint/syscalls/sys_enter_execve")
 int bpf_prog(void *ctx) {
